6f36cb34251dbfdfdfab5d3b15a81c4b20d93dfa
Richard Verbrugge <verbrugges.are@gmail.com>
2022-04-08 15:14:55 -0400

    Added function to track next and previous in iterable list.

--- functions.py
+++ functions.py
@@ -1,16 +1,26 @@
 import pygame
 import os
 import math
+from itertools import tee, islice, chain
 
 def load_image(dir, path):
     return pygame.image.load(os.path.join(dir, path))
 
+
 def get_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 
 def point_direction(x1, y1, x2, y2, radians = False):
     if radians:
         return math.atan2((y2 - y1), (x2 - x1))
     else:
         return math.atan2((y2 - y1), (x2 - x1)) * 180 / math.pi
+
+
+def previous_and_next(some_iterable):
+    prevs, items, nexts = tee(some_iterable, 3)
+    prevs = chain([None], prevs)
+    nexts = chain(islice(nexts, 1, None), [None])
+    return zip(prevs, items, nexts)
 
